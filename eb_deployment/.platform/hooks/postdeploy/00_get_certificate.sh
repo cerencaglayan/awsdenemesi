f9fafec01837909c8b5db5c670f5c83fd9253021
cerencaglayan <cer79cag@gmail.com>
2024-01-30 13:43:34 +0300

    üçüç

--- eb_deployment/.platform/hooks/postdeploy/00_get_certificate.sh
+++ eb_deployment/.platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 if ! grep -q letsencrypt </etc/nginx/nginx.conf; then
-  sudo certbot -n -d delta.eu-west-1.elasticbeanstalk.com --nginx --agree-tos --email scream_boy.s@hotmail.com
+  sudo certbot -n -d ssss.eu-north-1.elasticbeanstalk.com --nginx --agree-tos --email cer79cag@gmail.com
 fi
